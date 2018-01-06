9f45b03457af80f343e451c45e0a280a50201495
Juuso Mikkonen <hello@juusomikkonen.com>
2018-01-06 16:21:03 +0200

    Add basic CSS resets

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -22,6 +22,15 @@ injectGlobal`
     }
     body {
         font-family: Helvetica, sans-serif;
+        margin: 0;
+    }
+    html {
+        box-sizing: border-box;
+    }
+    *,
+    *:before,
+    *:after {
+        box-sizing: inherit;
     }
 `;
 
