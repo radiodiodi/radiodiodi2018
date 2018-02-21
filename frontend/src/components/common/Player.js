12fe773bcd974073fb05bd510d082fea49b7327e
Jan Tuomi <jan.tuomi@eficode.com>
2018-02-21 21:21:08 +0200

    Add funny spinning player

--- frontend/src/components/common/Player.js
+++ frontend/src/components/common/Player.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import PropTypes from 'prop-types';
 
 import play from '../../svg/play.svg';
+
+const Spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
 
 const Container = styled.div`
   background-color: ${p => p.theme.color.pink};
@@ -12,6 +22,8 @@ const Container = styled.div`
   @media screen and (max-width: 800px) {
     margin: 1rem 0 1rem;
   }
+
+  ${p => p.spinning && `animation: ${Spin} 2s normal forwards ease-in-out`};
 `;
 
 const PlayButton = styled.img`
@@ -25,17 +37,35 @@ const Line = styled.p`
 `;
 
 class Player extends Component {
+  constructor() {
+    super();
+    this.state = {
+      spinning: false,
+    };
+  }
+
+  spin = () => {
+    this.setState({
+      spinning: true,
+    });
+
+    setTimeout(() => this.setState({
+        spinning: false,
+    }), 2000);
+  }
+
   static contextTypes = {
     trans: PropTypes.any,
   }
 
   render() {
     const { trans } = this.context;
+    const { spinning } = this.state;
 
     return (
-      <Container>
+      <Container spinning={spinning} >
         <Line>{ trans.broadcastended }</Line>
-        <PlayButton src={ play } />
+        <PlayButton onClick={this.spin} src={ play } />
         <Line>{ trans.tunein }</Line>
       </Container>
     );
