45dc7451a9a44699818b55869d271a2c020567c3
Jan Tuomi <jan.tuomi@eficode.com>
2018-03-12 13:45:02 +0200

    Change bash to zsh when deploying

--- .travis_deploy.sh
+++ .travis_deploy.sh
@@ -1,2 +1,2 @@
 chmod 600 /tmp/deploy_key;
-ssh -oStrictHostKeyChecking=no -i /tmp/deploy_key "$DEPLOY_USER@$DEPLOY_SERVER" 'bash deploy_radiodiodi2018.sh';
+ssh -oStrictHostKeyChecking=no -i /tmp/deploy_key "$DEPLOY_USER@$DEPLOY_SERVER" 'zsh deploy_radiodiodi2018.sh';
