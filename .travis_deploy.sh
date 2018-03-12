28243cbe004fefc01dba98151f01b11dbfacc8e2
Jan Tuomi <jan.tuomi@eficode.com>
2018-03-12 13:31:23 +0200

    Move deploy logic to .travis_deploy.sh

---
+++ .travis_deploy.sh
@@ -0,0 +1,2 @@
+chmod 600 /tmp/deploy_key;
+ssh -oStrictHostKeyChecking=no -i /tmp/deploy_key "$DEPLOY_USER@$DEPLOY_SERVER" 'bash deploy_radiodiodi2018.sh';
--- deploy.sh
+++
@@ -1,6 +0,0 @@
-echo "$PRIVATE_KEY" > key;
-echo "$PUBLIC_KEY" > key.pub;
-chmod 600 key;
-eval "$(ssh-agent -s)";
-ssh-add key;
-ssh -oStrictHostKeyChecking=no -oBatchMode=yes "$DEPLOY_USER"@"$DEPLOY_SERVER" 'bash deploy_radiodiodi2018.sh';
