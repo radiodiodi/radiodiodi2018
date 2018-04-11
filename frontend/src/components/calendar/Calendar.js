b06e516f3787f181348b25b969330b312b1fc141
Juuso Mikkonen <hello@juusomikkonen.com>
2018-04-11 22:40:16 +0300

    Add (ugly) styling to calendar

--- frontend/src/components/calendar/Calendar.js
+++ frontend/src/components/calendar/Calendar.js
@@ -1,5 +1,46 @@
 import React from 'react';
+import styled from 'styled-components'
 import { groupBy } from 'lodash';
+import { shortenText } from '../../utils'
+
+const Program = styled.div`
+  background-color: ${p => p.theme.color.pink};
+  color: ${p => p.theme.color.dark};
+  padding: 0.5rem;
+  margin: 0.5rem;
+  min-height: 170px;
+`
+
+const ImagePlaceholder = styled.div`
+  height: 150px;
+  width: 150px;
+  float: right;
+  background-color: ${p => p.theme.color.dark};
+`
+
+const Img = ImagePlaceholder.withComponent('img')
+
+const Button = styled.button`
+  background-color: ${p => p.theme.color.pink};
+  color: ${p => p.theme.color.dark};
+  padding: 0.5rem;
+  font-size: 14px;
+  border: none;
+`
+
+const Title = styled.h4`
+  margin: 0.5rem 0;
+  font-size: 18px;
+  border-bottom: 1px solid ${p => p.theme.color.dark};
+`
+
+const Author = styled.p`
+  margin: 0.5rem 0;
+`
+
+const P = styled.p`
+  font-size: 12px;
+`
 
 class Calendar extends React.Component {
   constructor(props) {
@@ -27,13 +68,20 @@ class Calendar extends React.Component {
     if (!ready) return null
     return (
       <div>
+        <h2>Ohjelmakalenteri</h2>
+        <Button onClick={this.decrementDay}>Edellinen</Button>
+        <span>{today}.4.2018</span>
+        <Button onClick={this.incrementDay}>Seuraava</Button>
         {all[today] && all[today].map(p => (
-          <div className="chart-show">
+          <Program>
+            {p.image
+              ? <Img src={`https://radiodiodi.fi/static/img/${p.image}`} />
+              : <ImagePlaceholder />}
             <small>{p.start.substr(11, 5) + ' - ' + p.end.substr(11, 5)}</small>
-            <p>{p.title}</p>
-            <p className="chart-description">{p.team}</p>
-            <p className="chart-description">{p.description}</p>
-          </div>)
+            <Title>{p.title}</Title>
+            <Author>{p.team}</Author>
+            {p.description && <P>{shortenText(p.description, 200)}</P>}
+          </Program>)
         )}
       </div>
     )
