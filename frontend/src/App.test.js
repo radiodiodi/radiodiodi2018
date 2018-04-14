3f18b61b029fa1a3c2345e104eb98f3e0d5b0107
Jan Tuomi <jan.tuomi@eficode.com>
2018-04-14 17:20:59 +0300

    Fix tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -4,8 +4,12 @@ import fetch from 'isomorphic-fetch';
 import fetchMock from 'fetch-mock';
 import App from './components/App';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 it('renders without crashing', () => {
   fetchMock.get('begin:https://api.instagram.com', { data: [] });
+  fetchMock.get(`begin:${process.env.REACT_APP_BACKEND_HTTP_URL}/programmes`, { data: [] });
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   fetchMock.restore();
