e186232e330432600680d0930024936bd3c3f05c
Jan Tuomi <jan.tuomi@eficode.com>
2018-04-16 18:46:59 +0300

    Fix tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import 'isomorphic-fetch';
 import fetchMock from 'fetch-mock';
 import App from './components/App';
-
+import { WebSocket } from 'mock-socket';
 import dotenv from 'dotenv';
+
+global.WebSocket = WebSocket;
 dotenv.config();
 
 it('renders without crashing', () => {
